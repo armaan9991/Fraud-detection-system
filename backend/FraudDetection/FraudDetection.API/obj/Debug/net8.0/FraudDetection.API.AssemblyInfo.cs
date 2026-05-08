146f806c9f19d79a59ba41303af7026ed5a367d5
armaan9991 <armaangill9991@gmail.com>
2026-05-07 23:00:59 -0600

    fixing branches

--- backend/FraudDetection/FraudDetection.API/obj/Debug/net8.0/FraudDetection.API.AssemblyInfo.cs
+++ backend/FraudDetection/FraudDetection.API/obj/Debug/net8.0/FraudDetection.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FraudDetection.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef180846182fa8f390f738a99ddc1e1675e826c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FraudDetection.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FraudDetection.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
