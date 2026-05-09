179c7cbd55618c10c5491f5db0c2ad1bb672bd95
armaan9991 <armaangill9991@gmail.com>
2026-05-09 00:48:10 -0600

    implemented Transaction API endpoints

--- backend/FraudDetection/FraudDetection.API/Controllers/TransactionsController.cs
+++ backend/FraudDetection/FraudDetection.API/Controllers/TransactionsController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace FraudDetection.API.Controllers;
 
 [ApiController]
-[Route('api/[controller]')]
+[Route("api/[controller]")]
 public class TransactionsController: ControllerBase
 {
     private readonly ApplicationDbContext _context;
@@ -16,6 +16,61 @@ public class TransactionsController: ControllerBase
     public TransactionsController(ApplicationDbContext context)
     {
         _context = context;
+    }
+
+// post   api/transactions
+    [HttpPost]
+    public async Task<IActionResult> CreateTransaction(CreateTransactionDtos dto)
+    {
+         var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == dto.UserId);
+
+         if(user == null)
+        {
+            return NotFound("User is not present!");
+        }
+
+        var transaction = new Transaction
+        {
+            UserId = dto.UserId,
+            Amount = dto.Amount,
+            Currency = dto.Currency,
+            Country = dto.Country,
+            FraudScore = 0,
+            Status = "PENDING"
+        };
+
+        _context.Transactions.Add(transaction);
+
+        await _context.SaveChangesAsync();
+        
+        return CreatedAtAction(
+            nameof(getTransactionById),
+            new { id = transaction.TransactionId}, transaction
+        );
+    }
+
+
+    // get api/transaction
+
+    [HttpGet]
+    public async Task<IActionResult> GetTransactions()
+    {
+        var transactions =await _context.Transactions.Include(t =>t.User).ToListAsync();
+
+        return Ok(transactions);
+    }
+
+
+   [HttpGet("{id}")] 
+   public async Task<IActionResult> getTransactionById(int id)
+    {
+        var transaction = await _context.Transactions.Include(t=>t.User).FirstOrDefaultAsync(t=>t.UserId == id);
+
+        if (transaction == null)
+        {
+            return NotFound();
+        }
+        return Ok(transaction);
     }
 
 }
