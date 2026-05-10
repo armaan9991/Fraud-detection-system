539acb3d69d413cae32eca05a305a39b17ca3cf3
armaan9991 <armaangill9991@gmail.com>
2026-05-09 23:03:59 -0600

    implemented User service interface

---
+++ backend/FraudDetection/FraudDetection.API/Services/IUserService.cs
@@ -0,0 +1,10 @@
+using FraudDetection.API.Models;
+
+namespace FraudDetection.API.Services;
+
+public interface IUserService
+{
+    Task<List<User>> GetAllUsersAsync();
+    Task<User?> GetUserByIdAsync(int id);
+    Task<User> CreateUserAsync(User user);
+}
--- backend/FraudDetection/FraudDetection.API/obj/Debug/net8.0/FraudDetection.API.AssemblyInfo.cs
+++ backend/FraudDetection/FraudDetection.API/obj/Debug/net8.0/FraudDetection.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FraudDetection.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+813de4443263c2f65a8dd1b61ba380e7c3b0ea5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0222bbed7b9d1b49054b066bd71f18fb40c722")]
 [assembly: System.Reflection.AssemblyProductAttribute("FraudDetection.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FraudDetection.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
