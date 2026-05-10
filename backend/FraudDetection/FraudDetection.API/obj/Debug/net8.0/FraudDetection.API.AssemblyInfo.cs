c65c08298125bbbf01ff642419b3a31113c6e5a8
armaan9991 <armaangill9991@gmail.com>
2026-05-10 01:05:44 -0600

    removed generated files

--- backend/FraudDetection/FraudDetection.API/obj/Debug/net8.0/FraudDetection.API.AssemblyInfo.cs
+++ backend/FraudDetection/FraudDetection.API/obj/Debug/net8.0/FraudDetection.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FraudDetection.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+539acb3d69d413cae32eca05a305a39b17ca3cf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c2c482ba3678432501872453fd8b3b2117475a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FraudDetection.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FraudDetection.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
