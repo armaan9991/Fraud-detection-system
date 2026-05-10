a0ed1589937de51baa3782ff9d0485d75fca2239
armaan9991 <armaangill9991@gmail.com>
2026-05-10 01:27:56 -0600

    created FraudAlert interface

---
+++ backend/FraudDetection/FraudDetection.API/Services/IFraudAlertService.cs
@@ -0,0 +1,10 @@
+using FraudDetection.API.DTOs;
+using FraudDetection.API.Models;
+
+namespace FraudDetection.API.Services;
+public interface IFraudAlertService
+{
+    Task<List<FraudAlert>> GetAlertAsync();
+    Task<FraudAlert?> GetAlertByIdAsync(int id);
+    Task<FraudAlert?> CreateAlertAsync(CreateFraudAlertDto dto);
+}
