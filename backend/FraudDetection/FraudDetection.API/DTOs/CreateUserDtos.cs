433dd03996d4940e115d23ab03be305d4b359fe1
armaan9991 <armaangill9991@gmail.com>
2026-05-11 22:42:07 -0600

    added validation for creation of usre

--- backend/FraudDetection/FraudDetection.API/DTOs/CreateUserDtos.cs
+++ backend/FraudDetection/FraudDetection.API/DTOs/CreateUserDtos.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FraudDetection.API.DTOs;
 
 public class CreateUserDtos
 {
+
     [Required]
     [StringLength(100)]
     public string Name {get; set;} = string.Empty;
+    [Required]
+    [EmailAddress]
     public string Email {get; set;} = string.Empty;
 }
