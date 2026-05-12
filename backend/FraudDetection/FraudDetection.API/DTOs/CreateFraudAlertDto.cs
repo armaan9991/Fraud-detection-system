70959a39becf965f8ae005b3d6a52c8865041590
armaan9991 <armaangill9991@gmail.com>
2026-05-11 22:41:01 -0600

    added validation for creation of Fraud Alert

--- backend/FraudDetection/FraudDetection.API/DTOs/CreateFraudAlertDto.cs
+++ backend/FraudDetection/FraudDetection.API/DTOs/CreateFraudAlertDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FraudDetection.API.DTOs;
 
 public class CreateFraudAlertDto
 {
+        [Required]
         public int TransactionId {get;set;}
+         [Required]
+        [StringLength(20)]
         public string RiskLevel {get;set;} = string.Empty;
+         [Required]
+        [StringLength(200)]
         public string Reason {get;set;} =string.Empty;
 }
--- backend/FraudDetection/FraudDetection.API/DTOs/CreateUserDtos.cs
+++ backend/FraudDetection/FraudDetection.API/DTOs/CreateUserDtos.cs
@@ -2,6 +2,8 @@ namespace FraudDetection.API.DTOs;
 
 public class CreateUserDtos
 {
+    [Required]
+    [StringLength(100)]
     public string Name {get; set;} = string.Empty;
     public string Email {get; set;} = string.Empty;
 }
