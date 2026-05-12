3ae277bbe32167006a2ba71b1bb0fe0adf037bdb
armaan9991 <armaangill9991@gmail.com>
2026-05-11 22:43:31 -0600

    added validation for creation of device

--- backend/FraudDetection/FraudDetection.API/DTOs/CreateDeviceDtos.cs
+++ backend/FraudDetection/FraudDetection.API/DTOs/CreateDeviceDtos.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FraudDetection.API.DTOs;
 
 public class CreateDeviceDtos
 {
+    [Required]
+    [Range(1001,100000)]
     public int UserId {get;set;}
+    [Required]
+    [StringLength(100)]
     public string DeviceName {get;set;}=string.Empty;
+    [Required]
+    [StringLength(100)]
     public string IPAddress{get;set;}=string.Empty;
 }
