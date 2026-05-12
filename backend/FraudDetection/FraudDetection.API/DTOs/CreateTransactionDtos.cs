b41ee52d5c666b61424f303b0e1f5ae1d4b461b9
armaan9991 <armaangill9991@gmail.com>
2026-05-11 22:38:49 -0600

    added validation for creation of transaction\

--- backend/FraudDetection/FraudDetection.API/DTOs/CreateTransactionDtos.cs
+++ backend/FraudDetection/FraudDetection.API/DTOs/CreateTransactionDtos.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FraudDetection.API.DTOs;
 
 public class CreateTransactionDtos
 {
+    [Required]
+    [Range(1001, 1000000)]
     public int UserId {get;set;}
+    [Required]
+    [Range(1, 1000000)]
     public decimal Amount {get;set;}
+    [Required]
+    [StringLength(10)]
     public string Currency {get;set;}="CAD";
+    [Required]
+    [StringLength(50)]
     public string Country{get;set;}=string.Empty;
 }
