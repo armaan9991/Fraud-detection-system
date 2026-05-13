0e1187def3ac745d0eed245948d1a90a916088f1
armaan9991 <armaangill9991@gmail.com>
2026-05-13 00:07:26 -0600

    fast api

---
+++ ml/src/api.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+import joblib
+import numpy as np
+
+Model = joblib.load("../models/fraud_model.pkl")
+
+app = FastAPI()
+
+# Request schema
+class Transactiondata(BaseModel):
+    amount: float
+    isForeignTransaction : int
+    isNightTransaction : int
+    fraudScore: int
+
+# prediction endpoint
+@app.post("/predict")
+
+def predict_fraud(data: Transactiondata):
+    features = np.array([
+        [
+            data.amount,
+            data.isForeignTransaction,
+            data.isNightTransaction,
+            data.fraudScore
+        ]
+    ])
+
+    prediction = Model.predict(features)[0]
+
+    probabilty = Model.predict_proba(features)[0][1]
+
+    return {
+        "prediction" : int(prediction),
+        "fraudProbablity" : float(probabilty)
+    }
+
