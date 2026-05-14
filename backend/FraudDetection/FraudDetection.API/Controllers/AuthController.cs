ed96646df24d5c53599dc73e72164f500be86da3
armaan9991 <armaangill9991@gmail.com>
2026-05-14 01:06:45 -0600

    authication controller end point:

---
+++ backend/FraudDetection/FraudDetection.API/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+using FraudDetection.API.Models;
+using FraudDetection.API.DTOs;
+using FraudDetection.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace FraudDetection.API.Controllers;
+
+[ApiController]
+[Route("api/controller")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterDto dto)
+    {
+        var response = await _authService.RegisterAsync(dto);
+        if (response == null)
+        {
+            return BadRequest("User is already present!");
+        }
+        return Ok(response);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginDto dto)
+    {
+        var response = await _authService.LoginAsync(dto);
+        if (response == null)
+        {
+            return NotFound("no User found!");
+        }
+
+        return Ok(response);
+    }
+}
