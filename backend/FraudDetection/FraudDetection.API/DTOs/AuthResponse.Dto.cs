e35faa814f8df902d0ff2e4d7abf535c4cd81007
armaan9991 <armaangill9991@gmail.com>
2026-05-13 22:22:43 -0600

    created Authication response DTO

---
+++ backend/FraudDetection/FraudDetection.API/DTOs/AuthResponse.Dto.cs
@@ -0,0 +1,8 @@
+namespace FraudDetection.API.DTOs;
+
+public class AuthResponseDto
+{
+    public string Token{get;set;} = string.Empty;
+    public string Email{get;set;} = string.Empty;
+    public string Role{get;set;} = string.Empty;
+}
