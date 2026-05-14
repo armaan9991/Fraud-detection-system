ad8591bbfb357aec2319483782ed9077770df7ec
armaan9991 <armaangill9991@gmail.com>
2026-05-13 22:15:32 -0600

    updated user model

--- backend/FraudDetection/FraudDetection.API/Models/User.cs
+++ backend/FraudDetection/FraudDetection.API/Models/User.cs
@@ -7,6 +7,8 @@ public class User
     public int UserId {get; set ;}
     public string Name {get; set;}
     public string Email {get; set;}
+    public string PasswordHash {get;set;} = string.Empty;
+    public string Role{get;set;}="User";
     public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
 
     public List<Transaction> Transactions {get; set;} = new();
