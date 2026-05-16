d421af4bb69d0ca1a7ef1e280a6022a7017acf97
armaan9991 <armaangill9991@gmail.com>
2026-05-16 01:30:12 -0600

    implemented filter userDTO

---
+++ backend/FraudDetection/FraudDetection.API/DTOs/UserFilterDto.cs
@@ -0,0 +1,10 @@
+namespace FraudDetection.API.DTOs;
+
+public class UserFilterDto
+{
+    public string? Name {get; set;}
+    public string? Email {get; set;}
+    public string? Role{get;set;}
+    public DateTime? FromData {get;set;}
+    public DateTime? ToDate{get;set;}
+}
