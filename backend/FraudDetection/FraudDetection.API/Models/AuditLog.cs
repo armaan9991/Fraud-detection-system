07cc4fa0c61fbdf376c789747a70928e68624c7a
armaan9991 <armaangill9991@gmail.com>
2026-05-17 13:31:32 -0600

    created model for Audit Logs

---
+++ backend/FraudDetection/FraudDetection.API/Models/AuditLog.cs
@@ -0,0 +1,12 @@
+namespace FraudDetection.API.Models;
+
+public class AuditLog
+{
+    public int AuditLogId{get;set;}
+    public int? UserId {get;set;}
+    public string Action {get;set;} = string.Empty;
+    public string EntityType {get;set;} = string.Empty;
+    public int? EntityId {get;set;}
+    public string Details {get;set;} = string.Empty;
+    public DateTime CreatedAt{get;set;}= DateTime.UtcNow;   
+}
