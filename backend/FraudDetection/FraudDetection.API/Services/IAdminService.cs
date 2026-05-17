4052ddf394235ec6f5997fd5a17867f04f8494ba
armaan9991 <armaangill9991@gmail.com>
2026-05-17 13:21:19 -0600

    renamed method name

--- backend/FraudDetection/FraudDetection.API/Services/IAdminService.cs
+++ backend/FraudDetection/FraudDetection.API/Services/IAdminService.cs
@@ -4,7 +4,7 @@ namespace FraudDetection.API.Services;
 public interface IAdminService
 {
     Task<AdminStatsDto> GetStatsAsync();
-    Task<PagedResult<UserResponseDto>> GetUserTransactionAsync(int page,int pageSize, UserFilterDto filterDto);
+    Task<PagedResult<UserResponseDto>> GetUsersAsync(int page,int pageSize, UserFilterDto filterDto);
     Task<PagedResult<TransactionResponseDto>> GetUserTransactionAsync(int userId, int page, int pageSize);
     Task<TransactionResponseDto?> UpdateTransactionStatusAsync(int transactionId, string status);
 
