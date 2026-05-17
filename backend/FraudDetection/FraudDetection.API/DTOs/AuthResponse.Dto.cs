19c560f32c8c1e5b7582295048c1dc968f2b5d4b
armaan9991 <armaangill9991@gmail.com>
2026-05-17 16:16:55 -0600

    updated response dto for authorization

--- backend/FraudDetection/FraudDetection.API/DTOs/AuthResponse.Dto.cs
+++ backend/FraudDetection/FraudDetection.API/DTOs/AuthResponse.Dto.cs
@@ -2,7 +2,8 @@ namespace FraudDetection.API.DTOs;
 
 public class AuthResponseDto
 {
-    public string Token{get;set;} = string.Empty;
+    public string AccessToken{get;set;} = string.Empty;
+    public string RefreshToken{get;set;} = string.Empty;
     public string Email{get;set;} = string.Empty;
     public string Role{get;set;} = string.Empty;
 }
