0fe9254e3426f3f728dde7854fb2112cb2855418
armaan9991 <armaangill9991@gmail.com>
2026-05-21 01:06:07 -0600

    fixed some typos

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axois from 'axios';           // to call  http req.
 import { useAuthStore } from '../store/authstore';
-import { config } from 'zod/v4/core';
+import { Navigate, replace } from 'react-router-dom';
 
 const api = axois.create({
     baseURL :'http://localhost:5297/api',
@@ -13,3 +13,31 @@ api.interceptors.request.use((config) =>{
     }
     return config;
 });
+
+api.interceptors.response.use(                // here response has 2 values (successHandler , errorHandler)
+    (response) => response,
+    async (error) =>{
+        const original = error.config;
+        if(error.response?.status == 401 && !original._retry){            //if 401 error try to refresh and check if we had tried earlier or not.
+            original._retry = true;
+            const refreshToken = useAuthStore.getState().refreshToken;       // get refresh token
+            if (refreshToken){                                               // if refresh token is actually present
+                try{    
+                    const {data} = await axois.post('http://localhost:5297/api/Auth/refresh', {refreshToken});        // try to refresh  
+                    const newToken = data.data.accessToken;                                       // get accesstoken
+                    useAuthStore.getState().setAccessToken(newToken);                               // set new access token
+                    original.headers.Authorization = `Bearer ${newToken}`;                          // new jwt token
+                    return api(original);
+                }catch{
+                    useAuthStore.getState().logout();
+                    window.location.href='/login';
+                }
+            }
+            else{
+                useAuthStore.getState().logout();
+                <Navigate to='/login' replace/>;
+            }
+        }
+        return Promise.reject(error);
+    }
+)
