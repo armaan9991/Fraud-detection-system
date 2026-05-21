c17ea945abff34a36127ac4afd05e1c47c8d9f76
armaan9991 <armaangill9991@gmail.com>
2026-05-20 23:44:34 -0600

    implemented  interface for common types

--- frontend/src/types/common.types.ts
+++ frontend/src/types/common.types.ts
@@ -10,3 +10,44 @@ export interface PagedResult<T>{
     totalRecords : number;
     totalPages: number;
 }
+export interface AuthResponse{
+    accessToken: string;
+    refrehToken :string;
+    email : string;
+    role: string;
+}
+
+export interface Transaction{
+    transactionId: number;
+    userId : number;
+    amount : number;
+    currency : string;
+    country : string;
+    fraudScore: number;
+    transactionTime : string;
+}
+
+export interface Device{
+    deviceId : number;
+    userId: number;
+    deviceName: string;
+    ipAddress: string;
+    lastUsed:string;
+}
+export interface FraudAlert{
+    fraudAlertId: number;
+    transactionId: number;
+    riskLevel: string;
+    reason:string;
+    createdAt: string;
+}
+
+export interface User{
+    userId:number;
+    name:string;
+    email:string;
+    role:string;
+    createdAt:string;
+    isFlagged:boolean;
+    flagReason?:string;
+}
