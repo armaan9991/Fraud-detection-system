776739326a86a983054666c6131919d13a1318fb
armaan9991 <armaangill9991@gmail.com>
2026-05-21 01:49:42 -0600

    working on login page!

--- frontend/src/auth/LoginPage.tsx
+++ frontend/src/auth/LoginPage.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuthStore } from "../store/authstore";
 import { useState } from "react";
 import z from "zod";
-import type { schema } from "@hookform/resolvers/ajv/src/__tests__/__fixtures__/data.js";
+import {login} from '../api/authApi';
 
 const schema = z.object({
     email: z.string().email('enter valid email!'),
@@ -15,4 +15,15 @@ export default function LoginPage(){
     const setAuth = useAuthStore((state) =>state.setAuth);
     const {serverError,setServerError} = useState<string | null>(null);
 
+    const onSubmit = async (data :LoginForm) => {
+        setServerError(null);
+        try{
+            const result = await login(data.email,data.password)
+            setAuth(result.accessToken, result.refreshToken, result.email, result.role);
+            naviagte('/dashboard');
+        }
+        catch{
+            setServerError('Invalid Email or password');
+        }
+    }
 }
