dc38ef29a38ad4692d21af07c566ee1a52ac3bc9
armaan9991 <armaangill9991@gmail.com>
2026-05-20 23:31:26 -0600

    added pagedresult interface

--- frontend/src/types/common.types.ts
+++ frontend/src/types/common.types.ts
@@ -3,3 +3,10 @@ export interface ApiResponse<T>{
     message: string;
     data : T;
 }
+export interface PagedResult<T>{
+    items : T[];
+    page : number;
+    pageSize : number;
+    totalRecords : number;
+    totalPages: number;
+}
