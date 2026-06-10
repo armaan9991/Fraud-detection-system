e1f842f366d6c04109e9fdf6099dfe5a2fb5d917
armaan9991 <armaangill9991@gmail.com>
2026-06-10 13:10:56 -0600

    added unflag user api

--- frontend/src/api/adminApi.ts
+++ frontend/src/api/adminApi.ts
@@ -1,5 +1,5 @@
 import { api } from "./axiosInstance";
-import type { ApiResponse,AdminStats, PagedResult,User } from "../types/common.types";
+import type { ApiResponse,AdminStats, PagedResult,User, UserResponseDto} from "../types/common.types";
 
 export const getAdminStats = async() :Promise<AdminStats> =>{
     const resp = await api.get<ApiResponse<AdminStats>>('/Admin/stats');
@@ -8,4 +8,16 @@ export const getAdminStats = async() :Promise<AdminStats> =>{
 export const getAdminUser = async(page:number, pagesize:number) : Promise<PagedResult<User>> =>{
     const resp = await api.get<ApiResponse<PagedResult<User>>>('/Admin/users',{params:{page,pagesize}});
     return resp.data.data;
+}
+export const unflagUser = async(userId:number) : Promise<UserResponseDto | null> =>{
+    try{
+    const resp = await api.patch<ApiResponse<UserResponseDto>>(`/Admin/users/${userId}/unflag`);
+    return resp.data.data;
+    }
+    catch (err:any){
+        if(err.response?.status === 404){
+            return null;
+        }
+        throw err;
+    }
 }
