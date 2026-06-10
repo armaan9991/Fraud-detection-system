943c872f4614ae3e26b9a5896afa73633ef36698
armaan9991 <armaangill9991@gmail.com>
2026-06-10 12:54:34 -0600

    get pagination admin user api

--- frontend/src/api/adminApi.ts
+++ frontend/src/api/adminApi.ts
@@ -1,7 +1,11 @@
 import { api } from "./axiosInstance";
-import type { ApiResponse,AdminStats } from "../types/common.types";
+import type { ApiResponse,AdminStats, PagedResult,User } from "../types/common.types";
 
 export const getAdminStats = async() :Promise<AdminStats> =>{
     const resp = await api.get<ApiResponse<AdminStats>>('/Admin/stats');
+    return resp.data.data;
+}
+export const getAdminUser = async(page:number, pagesize:number) : Promise<PagedResult<User>> =>{
+    const resp = await api.get<ApiResponse<PagedResult<User>>>('/Admin/users',{params:{page,pagesize}});
     return resp.data.data;
 }
