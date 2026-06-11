36b2d8191215e229bb9111547f244d2dc215d0eb
armaan9991 <armaangill9991@gmail.com>
2026-06-10 22:45:47 -0600

    implemented cross validation.

--- ml/src/train_model.py
+++ ml/src/train_model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
@@ -32,30 +32,36 @@ legit_samples = pd.DataFrame([{
   for h in [9, 10, 14, 15]])
 
 df = pd.concat([df, fraud_sample, legit_samples], ignore_index=True)
+print(f"Total: {len(df)}, Fraud: {df['isFraud'].sum()}, Legit: {(df['isFraud']==0).sum()}")
 
 #  Features
 X = df [[
     "amount",
     "isForeignTransaction",
-    "isNightTransaction"    ]]
+    "isNightTransaction",
+    "hour",
+    "isNonCadCurrency"
+    ]]
 
 #   Label
 y = df["isFraud"]
 
 
-X_train,X_test, y_train, y_test =(train_test_split(X,y,test_size=0.2,random_state=42))
+X_train,X_test, y_train, y_test =(train_test_split(X,y,test_size=0.2,random_state=42,stratify=y))
 
-model = RandomForestClassifier(class_weight='balanced', n_estimators=100, random_state=42)
+model = RandomForestClassifier(class_weight='balanced', n_estimators=200, random_state=42,max_depth=8)
 
 model.fit(X_train,y_train)
 
+## Cross Validation
+cv_scores = cross_val_score(model,X,y,cv=5,scoring='f1')
+print(f"CV F1 scores: {cv_scores.round(3)}, Mean: {cv_scores.mean():.3f}")
+
 predictions = model.predict(X_test)
 
-accuracy = accuracy_score(y_test,predictions)
+print(f"Accuracy: {accuracy_score(y_test, predictions):.3f}")
 
-print(f"Accuracy: {accuracy}")
-
-print( classification_report ( y_test , predictions))
+print(classification_report(y_test, predictions))
 
 # save model
 joblib.dump(model , "../models/fraud_model.pkl")
