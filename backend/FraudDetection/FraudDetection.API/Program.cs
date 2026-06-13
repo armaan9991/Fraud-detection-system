850d075b0227acc7891f333166e4311eb2fbb7d6
armaan9991 <armaangill9991@gmail.com>
2026-06-12 18:25:15 -0600

    registered passwordhash DI

--- backend/FraudDetection/FraudDetection.API/Program.cs
+++ backend/FraudDetection/FraudDetection.API/Program.cs
@@ -79,6 +79,7 @@ builder.Services.AddScoped<IRefreshTokenCleanupJob,RefreshTokenCleanupJob>();
 builder.Services.AddScoped<IDailyFraudReportJob,DailyFraudReportJob>();
 builder.Services.AddScoped<IHighRiskUserDetectionJob,HighRiskUserDetectionJob>();
 builder.Services.AddScoped<IEmailService,EmailService>();
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(option =>
 {
