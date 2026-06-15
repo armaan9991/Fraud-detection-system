bb2afd6d31e73d6dd094db5fda5a76e90ad1d6f6
armaan9991 <armaangill9991@gmail.com>
2026-06-14 21:55:27 -0600

    email fix.

--- backend/FraudDetection/FraudDetection.API/Services/AuthService.cs
+++ backend/FraudDetection/FraudDetection.API/Services/AuthService.cs
@@ -52,19 +52,21 @@ public class AuthService : IAuthService
         await _context.SaveChangesAsync();
         var emailbody =$"Hello {user.Name} \n Thankyou for registering .\n  Always ready to detect fraud transaction.\n Receive Live notifications and tracking of transactions. \n";
         
-       _ = Task.Run(async () =>
-    {
-     try
+    //    _ = Task.Run(async () =>
+    //     {
+        try
         {
-        await _emailService.SendEmailAsync(user.Email, "Created New User", emailbody);
+            await _emailService.SendEmailAsync(user.Email, "Created New User", emailbody);
+            Console.Write("email sent!");
         }
-        catch (Exception e)
-    {       
-    Console.WriteLine("EMAIL FAILED: " + e.Message);
-    Console.WriteLine("STACK: " + e.StackTrace);
-    }   
-
-    });
+            catch (Exception e)
+        {       
+            Console.WriteLine("EMAIL FAILED: " + e.Message);    
+            Console.WriteLine("STACK: " + e.StackTrace);
+            await _auditLogService.CreateLogAsync(user.UserId,"Failed to register User!","User",user.UserId,"failed to send user!!");
+        }   
+    // }
+    // );
 
         await _auditLogService.CreateLogAsync(user.UserId, "registered user","user",null,"created new user!");
         string Accesstoken = GenerateJwtToken(user);
